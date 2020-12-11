72256e31421700f2246a026e484c7a7cf74605d6
Rajanish <rajanishatwork@gmail.com>
2020-12-12 03:29:33 +0530

    initial commit
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

---
+++ src/test/java/com/shopskill/artifact/ArtifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopskill.artifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
